9864e0a353b9cb7816dffd149f565931fbf22011
thangpdgcd <thangpdgcd201784@fpt.edu.vn>
2024-03-25 14:21:42 +0700

    update file

--- src/AbtractList.java
+++ src/AbtractList.java
@@ -1,6 +1,11 @@
 public interface AbtractList<E> extends Iterable<E> {
-    boolean add(E element);
-boolean add ( int index);
-    E remove(int index);
+    boolean add( int element);
+    boolean add(int index, int element);
+    int get (int index);
+    int set(int index, int element);
+    int remove(int index);
     int size();
+    int indexOf(int element);
+    boolean contains(int element);
+    boolean isEmpty();
 }
